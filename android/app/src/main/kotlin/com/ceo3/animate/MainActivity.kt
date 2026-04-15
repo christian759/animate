f3e53043ef12377711796820e760378fea052881
ceo1 <christian4onos@outlook.com>
2026-04-15 15:17:34 +0100

    changing my tech stack

--- android/app/src/main/kotlin/com/ceo3/animate/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.ceo3.animate
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- scratch_gif.dart
+++
@@ -1,8 +0,0 @@
-import 'package:image/image.dart' as img;
-
-void main() {
-  final encoder = img.GifEncoder();
-  final image = img.Image(width: 10, height: 10);
-  final gif = encoder.encode(image);
-  print('Encoded size: ${gif.length}');
-}
