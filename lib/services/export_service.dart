a8cf9153978a12a35099835a05e51bfb6d331451
ceo1 <christian4onos@outlook.com>
2026-04-15 03:53:03 +0100

    refactor: remove ffmpeg_kit dependency and disable video/GIF export functionality

--- lib/services/export_service.dart
+++ lib/services/export_service.dart
@@ -5,8 +5,6 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:path/path.dart' as p;
-import 'package:ffmpeg_kit_flutter_min_gpl/ffmpeg_kit.dart';
-import 'package:ffmpeg_kit_flutter_min_gpl/return_code.dart';
 import '../models/models.dart';
 import '../ui/canvas_painter.dart';
 
@@ -56,39 +54,8 @@ class ExportService {
         return frameDir.path;
       }
 
-      // 2. Encode to requested format using FFmpeg
-      final outputExt = format == ExportFormat.gif ? 'gif' : 'mp4';
-      final outputFile = p.join(tempDir.path, 'export_$exportId.$outputExt');
-      
-      String ffmpegCommand;
-      if (format == ExportFormat.gif) {
-        ffmpegCommand = '-y -framerate ${project.fps} -i ${frameDir.path}/frame_%05d.png -vf "scale=720:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" $outputFile';
-      } else {
-        // Video export with potential audio
-        final hasAudio = project.audioPath != null && File(project.audioPath!).existsSync();
-        
-        if (hasAudio) {
-          ffmpegCommand = '-y -framerate ${project.fps} -i ${frameDir.path}/frame_%05d.png -i "${project.audioPath}" '
-              '-vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" -c:v libx264 -pix_fmt yuv420p -c:a aac -shortest $outputFile';
-        } else {
-          ffmpegCommand = '-y -framerate ${project.fps} -i ${frameDir.path}/frame_%05d.png '
-              '-vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" -c:v libx264 -pix_fmt yuv420p $outputFile';
-        }
-      }
-
-      debugPrint('Running FFmpeg: $ffmpegCommand');
-      
-      final session = await FFmpegKit.execute(ffmpegCommand);
-      final returnCode = await session.getReturnCode();
-
-      if (ReturnCode.isSuccess(returnCode)) {
-        onProgress?.call(1.0);
-        return outputFile;
-      } else {
-        final logs = await session.getLogs();
-        debugPrint('FFmpeg failed with return code $returnCode');
-        return null;
-      }
+      debugPrint('FFmpeg has been removed. Video/GIF export is currently unavailable.');
+      return null;
     } catch (e) {
       debugPrint('Export error: $e');
       return null;
